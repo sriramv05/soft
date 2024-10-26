71fe65e490b4a7e1b99b9248ed4201a0b90d64d5
sriramv05” git config --global user.email sram47527@gmail.com” <sriramv@SRIRAMs-MacBook-Air.local>
2024-10-26 12:22:36 +0530

    Initial commit

---
+++ config/config.dev.js
@@ -0,0 +1,8 @@
+window.config = {
+    name: 'SRIRAM V',
+    title: 'Backend Developer',
+    bio: 'I am a passionate student eho is eager to work on real-time application I have also doone my own projects with the python framework like FLask, Django and I have created basic website with different concepts.',
+    location: 'Coimbatore, TamilNadu, India',
+    email: 'sriramvelmurugan.05@gmail.com',
+    image: 'avatar.jpg'
+}
